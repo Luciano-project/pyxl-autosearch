99fe7bac3ff4688069b417f0ed20dcdef4e05d1f
Luciano-project <luhugo.lhcs@gmail.com>
2024-11-25 20:49:13 +0000

    refactor: the functions were refactored

--- api/base.py
+++ api/base.py
@@ -1,28 +1,25 @@
 
 #from configs.config import Setup
-import os, re
+import sys, os, re
 
-
-class Setup:
-    def __init__(self):
-        self.path = rf"C:\Users\lucia\Projects\python\tests" #Path de procura
-        self.root_dir = "PROJECTOS CLIENTES"
-        self.base_str_find = [] # STRINGS MUST BE INCLUDED IN NAME FILE
-        self.ignored_str = ["~$"]
-
+# Adiciona o diretório pai ao sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from configs.config import Setup
+from middlewares.openxl_files import OpenxlFiles
 
 
 class SearchFile(Setup):
-    def __init__(self, files = [{"namefile":"2301999", "coodenate":"C1", "value":""}, {"namefile":"2301999", "coodenate":"C1", "value":"Olá mundo"}]):
+    def __init__(self, files = [{"namefile":"2409999", "coordenate":"C1", "value":""}, {"namefile":"2409999", "coordenate":"C1", "value":"Olá mundo"}]):
         super().__init__()
         self.files = files
+        self.openxl_files = OpenxlFiles
 
     def search(self):
         '''Search files in path'''
         for root, dirs, files in os.walk(self.path, topdown=False):
             for name in files:
                 path = os.path.join(root, name)
-                print(path)
+                #print(path)
                 self.check_path(name, path, self.files)
                 if not len(self.files): return 0
 
@@ -31,41 +28,30 @@ class SearchFile(Setup):
         Check if file is in search list (in_search)
         '''
         for count, item in enumerate(in_search):
-            print("count:", count, "item:", item)
-            if self.search_in(item, name_file) and self.ignored_files_str(path)!=1:
+            if self.search_in(item['namefile'], name_file) and self.ignored_files_str(path)!=1:
                 self.proc_load_list(path, name_file, count)
                 self.check_path(name_file, path, in_search)
 
-    def proc_load_list(self, path, name, count):
-        if self.process_xl(path, self.files[count][1], self.files[count][0]): pass
+
+    def proc_load_list(self, path, name, count, method:str):
+        """
+        Here is where decision to processing the file is made, read or write. By using the parameter method: write, read.
+        """
+        if method == "write": self.write_file(path, name)
+        elif method == "read": self.read_file(path, name)
+        method(path, name, count)
+        
+        if self.process_xl(path, self.files[count]["coordenate"], self.files[count]["value"]): pass
             #self.save_file_status(path, self.files[count][1], self.files[count][0])       
         self.remove_found(count)
 
     def remove_found(self, count): self.files.pop(count)  
 
-    def search_base(self, name) -> None:
-        for item in self.base_str_find:
-            if not self.search_in(item, name):
-                return 0
-        return 1
-
-    def search_in(self, file_name:str, haystack:str) -> None:
-        if re.search(f"{file_name}", haystack, re.IGNORECASE):
-            return 1
-
-#REFATORATING METHODS START
-    def process_xl(self, path, of, nbr) -> None:
-        try:
-            return 1
-
-        except Exception as e:
-            self.save_file_status(path, of, nbr, e)
-            return 0
-
-    def validate_final_position(self, name_file, path, in_search):
-        final_position = name_file.split(".xlsx")[0].split(" ")[-1]
-        if final_position==in_search[0].upper(): return 0
-        return 1
+    def search_in(self, file_name:str, haystack:str) -> int:
+        '''Search if file_name is in haystack'''
+        if file_name in [None, ""] or haystack in [None, ""]: return 0
+        if re.search(f"{file_name}", haystack, re.IGNORECASE): return 1
+        return 0
 
 #IGNORED ITENS METHODS
     def ignored_files_str(self, haystack):
@@ -84,7 +70,7 @@ class SearchFile(Setup):
         return 0
 
     def save_file_status(self, path, of, nbr, e = ""):
-        self.status_file.append({"of" : f"{of}", "peca" : f"{nbr}", "caminho" : f"{path}","log":f"{e}"})
+        #self.status_file.append({"of" : f"{of}", "peca" : f"{nbr}", "caminho" : f"{path}","log":f"{e}"})
         if e!="":
             #conec.add_record(of, nbr, path, str(e))
             #self.connection_on=1
@@ -92,5 +78,4 @@ class SearchFile(Setup):
 
 if __name__ == "__main__":
     search = SearchFile()
-    print(search.files[0])
     search.search()
