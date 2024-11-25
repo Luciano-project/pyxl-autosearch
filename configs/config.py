bc85bc11c15af45c368e0a292d9435d2d92036a4
Luciano-project <luhugo.lhcs@gmail.com>
2024-11-25 22:46:18 +0000

    refactor: Removed unused variable and unecessary code

--- configs/config.py
+++ configs/config.py
@@ -6,14 +6,5 @@ SECRET_KEY = 'your_secret_key'
 class Setup:
     def __init__(self):
         self.path = rf"C:\Users\lucia\Projects\python\tests" #Path de procura
-        self.root_dir = "PROJECTOS CLIENTES"
         self.base_str_find = [] # STRINGS MUST BE INCLUDED IN NAME FILE
-
-
-        #for list status
-"""        self.pending_path=rf"{self.server_path}\jet 3\LISTAS"
-        self.loaded_path=rf"{self.server_path}\jet 3\LISTAS\Tratados"
-        self.repeated_path=rf"{self.server_path}\jet 3\LISTAS\OFsRepetidas"
-        self.rejected_path=rf"{self.server_path}\jet 3\LISTAS\FicheirosRejeitados"
-        self.invalid_path=rf"{self.server_path}\jet 3\LISTAS\FicheirosInvalidos"
-"""
+        self.ignored_str = []
