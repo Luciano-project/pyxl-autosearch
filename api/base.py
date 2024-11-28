61f8f4bffab049ee4223621363f8b9edc4e3a2c1
Luciano-project <luhugo.lhcs@gmail.com>
2024-11-28 11:50:10 +0000

    feat: functions to handle the not found search and add information to the payload

--- api/base.py
+++ api/base.py
@@ -1,42 +1,47 @@
 #from configs.config import Setup
+from configs.settings import Setup
+import logging
 import sys, os, re
 
 # Adiciona o diretório pai ao sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from configs.config import Setup
 
+logger = logging.getLogger(__name__)
 
 class SearchFile(Setup):
-    def __init__(self, files = [{"namefile":"2409999", "coordenate":"C1", "value":""}, {"namefile":"2409999", "coordenate":"C1", "value":"Olá mundo"}]):
+    def __init__(self, files:list[dict]):
         super().__init__()
         self.files = files
+        self.return_values = []
 
     def search(self):
         '''Search files in path'''
         for root, dirs, files in os.walk(self.path, topdown=False):
-            for name in files:
+            for name in files: 
                 path = os.path.join(root, name)
                 #print(path)
                 self.check_path(name, path, self.files)
                 if not len(self.files): return 0
+        self.handle_not_found()
 
     def check_path(self, name_file, path, in_search:list[dict]):
         '''
         Check if file is in search list (in_search)
         '''
         for index_list, item in enumerate(in_search):
-            if self.search_in(item['namefile'], name_file) and self.ignored_files_str(path)!=1:
-                self.proc_load_list(path, name_file, index_list)
+            if self.search_in(item['filename'], name_file) and self.ignored_files_str(path)!=1:
+                sheetname = item['sheetname'] if 'sheetname' in item else self.default_sheetname
+                self.proc_load_list(path, name_file, index_list, sheetname = sheetname)
                 self.remove_found(index_list)
                 self.check_path(name_file, path, in_search)
 
-    def proc_load_list(self, path, name_file, index_list):
+    def proc_load_list(self, path, name_file, index_list, sheetname):
         """
         Here is where decision to processing the file is made, read or write. By using the parameter method: write, read.
         ~ This function will be overrided in the child class from views.
         """
         pass
-        
+
     def remove_found(self, index_list): self.files.pop(index_list)  
 
     def search_in(self, file_name:str, haystack:str) -> int:
@@ -45,19 +50,25 @@ class SearchFile(Setup):
         if re.search(f"{file_name}", haystack, re.IGNORECASE): return 1
         return 0
 
+    def handle_not_found(self):
+        '''Handle files not found'''
+        for item in self.files:
+            self.insert_data_response({"filename": item["filename"],
+                                "coordinate": item["coordinate"],
+                                "value": item["value"] if "value" in item else "Not found",
+                                "path": "Not found",
+                                "error": "Not found",
+                                })
+        return 0
+
+    def insert_data_response(self, values:dict):
+        '''insert data to the respose list'''
+        self.return_values.append(values)
+
 #IGNORED ITENS METHODS
     def ignored_files_str(self, haystack):
         for item in self.ignored_str:
             if(self.search_in(item, haystack) == 1):
-                return 1
-        return 0
-
-    def not_substr(self, needle, haystack):
-        values_fmt1 = haystack.replace("_"," ")
-        values_list = values_fmt1.split(" ")
-        needle = needle.upper()
-        for item in values_list:
-            if needle == item:
                 return 1
         return 0
 
