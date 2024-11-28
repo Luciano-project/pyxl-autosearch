82d434a7224c550d77c5077326639d5be393f806
Luciano-project <luhugo.lhcs@gmail.com>
2024-11-28 11:52:07 +0000

    refactor: Changes in the call of the methods was implemented and route was renamed

--- api/routes.py
+++ api/routes.py
@@ -1,23 +1,24 @@
 from flask import Flask, request
 from flask_cors import CORS
 from flask import make_response
+from .views import *
 
 
 api = Flask(__name__)
 CORS(api, supports_credentials=True)
 
-@api.route('/pyxl/update', methods=['POST'])
+@api.route('/pyxl/write', methods=['PATCH'])
 def update():
-    if request.method == 'POST' :
-        print("POST")
-        return 200
+    if request.method == 'PATCH' :
+        data = write_file(request.json)
+        return make_response(data, 200)
     
-    else: make_response("Method not allowed", 405)
+    else: make_response("Not found", 404)
 
 @api.route('/pyxl/read', methods=['POST'])
 def read():
-    if request.method == 'POST' :
-        print("POST")
-        return 200
+    if request.method == 'POST':
+        data = read_file(request.json)
+        return make_response(data, 200)
     
-    else: make_response("Method not allowed", 405)
+    else: make_response("Not found", 404)
