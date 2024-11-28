dea1eeb0144913c53bb01a1fef37617cdc5201f9
Luciano-project <luhugo.lhcs@gmail.com>
2024-11-28 16:46:08 +0000

    feat: The functional parts were implemented in the views

--- api/views.py
+++ api/views.py
@@ -3,33 +3,44 @@ from middlewares.openxl_files import OpenxlFiles
 
 
 class ReadFile(base.SearchFile):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, files):
+        super().__init__(files)
         self.sheetname = None
-    
-    def proc_load_list(self, path, name_file, index_list, sheetname):
+
+    def proc_load_list(self, path, name_file, index_list, sheetname=None):
         xl_file = OpenxlFiles(path)
-        self.sheetname = sheetname
-        xl_file.open_file(path)
+        self.sheetname = self.default_sheetname if sheetname == None else sheetname
+        xl_file.open_file(path, data_only=True)
         xl_file.set_wb_sheet(sheetname)
-        xl_file.get_data(self.files[index_list]["coordenate"])
+        value = None
+        try:
+            value = xl_file.get_value(self.files[index_list]["coordinate"])
+            self.insert_data_response({"value": value, "path":path, "coordinate": self.files[index_list]["coordinate"]})
+        except Exception as e:
+            self.insert_data_response({"value": "", "path":path, "coordinate": self.files[index_list]["coordinate"], "error": e})
 
 class WriteFile(base.SearchFile):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, files):
+        super().__init__(files)
         self.sheetname = None
     
-    def proc_load_list(self, path, name_file, index_list, sheetname):
-        xl_file = OpenxlFiles(path)
-        self.sheetname = sheetname
+    def proc_load_list(self, path, name_file, index_list, sheetname=None):
+        xl_file = OpenxlFiles(path)        
+        self.sheetname = self.default_sheetname if sheetname == None else sheetname
         xl_file.open_file(path)
         xl_file.set_wb_sheet(sheetname)
-        xl_file.update_data(self.files[index_list]["coordenate"], self.files[index_list]["value"])
+        old_value = xl_file.get_value(self.files[index_list]["coordinate"])
+        xl_file.update_value(self.files[index_list]["coordinate"], self.files[index_list]["value"])
+        self.insert_data_response({"old_value": old_value, "value": self.files[index_list]["value"], "path":path})
         xl_file.close_file()
         xl_file.save_file(path)
 
-def update_file(files:list[dict]):
-    file = ReadFile(files = dict)
+def write_file(files:list[dict]):
+    file = WriteFile(files)
+    file.search()
+    return file.return_values
 
 def read_file(files:list[dict]):
-    file = WriteFile(files = dict)
+    file = ReadFile(files)
+    file.search()
+    return file.return_values
