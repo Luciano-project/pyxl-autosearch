5df948342f1de3c0e66d034cb23156ea5d597a24
Luciano-project <luhugo.lhcs@gmail.com>
2024-12-01 01:25:16 +0000

    feat: added checks and try-exceptions to validate

--- api/views.py
+++ api/views.py
@@ -31,22 +31,32 @@ class WriteFile(base.SearchFile):
         if is_coordinate_cell_valid(self.files[index_list]["coordinate"]) == False:
             self.handle_invalid_ref(self.files[index_list])
             return 0
-        xl_file = OpenxlFiles(path)
-        self.sheetname = self.default_sheetname if sheetname == None else sheetname
-        xl_file.open_file(path)
-        xl_file.set_wb_sheet(sheetname)
-        old_value = xl_file.get_value(self.files[index_list]["coordinate"])
-        xl_file.update_value(self.files[index_list]["coordinate"], self.files[index_list]["value"])
         try:
-            self.insert_data_response({"old_value": old_value, "coordinate":self.files[index_list]["coordinate"], "value": self.files[index_list]["value"], "path":path})
-            xl_file.save_file(path)
-            xl_file.close_file()
-            return 1
+            xl_file = OpenxlFiles(path)
+            self.sheetname = self.default_sheetname if sheetname == None else sheetname
+            xl_file.open_file(path)
+            xl_file.set_wb_sheet(sheetname)
+            check_merged = self.check_merged_cell(self.files[index_list]["coordinate"], xl_file.get_wb_sheet())
+            old_value = xl_file.get_value(self.files[index_list]["coordinate"]) if check_merged == 0 else check_merged
+            xl_file.update_value(self.files[index_list]["coordinate"], self.files[index_list]["value"]) if check_merged == 0 else check_merged
+            try:
+                self.insert_data_response({"old_value": old_value, "coordinate":self.files[index_list]["coordinate"] if check_merged == 0 else check_merged, "value": self.files[index_list]["value"], "path":path})
+                xl_file.save_file(path)
+                xl_file.close_file()
+                return 1
+            
+            except Exception as e:
+                self.insert_data_response({"old_value": old_value, "coordinate": self.files[index_list]["coordinate"], "value": "", "path": path, "error": f"Trying to write data and {e}"})
+                xl_file.close_file()
+                return 0
+
+        except AttributeError as e:
+            self.insert_data_response({"value": "", "path":path, "coordinate": self.files[index_list]["coordinate"], "error": "Merged Cell, not possible to write in this cell"})
+            return 0
         
         except Exception as e:
-            self.insert_data_response({"old_value": old_value, "coordinate": self.files[index_list]["coordinate"], "value": "", "path": path, "error": e})
+            self.insert_data_response({"value": "", "path":path, "coordinate": self.files[index_list]["coordinate"], "error": e})
             return 0
-
         
 
 def write_file(files:list[dict]):
