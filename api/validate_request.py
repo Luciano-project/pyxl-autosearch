05e4cd0ff7367cf278bf8ad9e588ce60331b23f7
Luciano-project <luhugo.lhcs@gmail.com>
2024-12-03 14:19:14 +0000

    fix: a validation for the 'savepath' was implemented

--- api/validate_request.py
+++ api/validate_request.py
@@ -7,16 +7,20 @@ def is_coordinate_cell_valid(coordinate:str) -> int:
     if re.search(fr'^[A-Z]+[0-9]+$', coordinate): return True
     return False
 
-def is_savepath_exists(savepath:str) -> int:
+def is_savepath_exists(body_request:dict) -> int:
     '''Check if savepath exists'''
-    if os.path.exists(savepath):
-        return 1
+    is_requested_filepath_in_body = is_required_data_in_request(body_request, "savepath")
+    if is_requested_filepath_in_body == 0: return 1
+    if os.path.exists(body_request["filepath"]): return 1
+    return 0
+
+def is_required_data_in_request(request:dict, required_data:str) -> int:
+    '''Check if required data exists in request'''
+    if required_data in request.keys(): return 1
     return 0
 
 def is_request_valid(request:dict) -> int:
     '''Check if request is valid'''
-
     required_data = ["coordinate", "filename"]
     if set(required_data).issubset(request.keys()): return 1
-
     return 0
