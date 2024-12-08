4869ade8ec4a5ea48862bba969eb4df3df654e1d
Luciano-project <luhugo.lhcs@gmail.com>
2024-12-08 23:33:38 +0000

    feat: Added a validation

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from . import base
 from .middlewares.openxl_files import OpenxlFiles
-from .validate_request import is_coordinate_cell_valid, is_savepath_exists, is_request_valid
+from .validate_request import is_coordinate_cell_valid, is_savepath_exists, is_request_valid, is_required_data_in_request
 
 class ReadFile(base.SearchFile):
     def __init__(self, files):
@@ -52,7 +52,7 @@ class WriteFile(base.SearchFile):
         try:
             xl_file = OpenxlFiles(path)
 
-            if is_savepath_exists(self.files[index_list]["savepath"]) == 0:
+            if is_savepath_exists(self.files[index_list]) == 0:
                 self.handle_invalid_path(self.files[index_list])
                 return 0
             
@@ -62,7 +62,6 @@ class WriteFile(base.SearchFile):
             if xl_file.is_sheetname_valid(sheetname, xl_file) == 0:
                 self.handle_invalid_sheetname(self.files[index_list])
                 return 0
-            
             xl_file.set_wb_sheet(sheetname)
             check_merged = self.check_merged_cell(self.files[index_list]["coordinate"], xl_file.get_wb_sheet())
             old_value = xl_file.get_value(self.files[index_list]["coordinate"]) if check_merged == 0 else xl_file.get_value(check_merged)
@@ -73,7 +72,6 @@ class WriteFile(base.SearchFile):
                                            "path": path,
                                            "old_value": old_value,
                                            "error": "" if check_merged == 0 else f"It is a warning, the cell is merged and the value was written in the first cell of the merged cell: {check_merged}"})
-                is_requested_savepath = 1 if self.files[index_list]["savepath"] else 0
                 
                 to_savepath = self.check_savepath_and_return_if_valid(self.files[index_list])
                 if to_savepath == -1: raise Exception("Savepath is invalid") if to_savepath == 0 else None
@@ -94,6 +92,15 @@ class WriteFile(base.SearchFile):
                                        "path": path,
                                        "error": f"Check the requested data. {e}"})
             return 0
+    
+    def check_savepath_and_return_if_valid(self, requested_item:dict):
+        '''Check if savepath exists in request and return\n
+        if it does not exist, try the default savepath\n
+        else return 0'''
+        if is_required_data_in_request(requested_item, "savepath") == 1: return requested_item["savepath"]
+        elif self.default_savepath: return self.default_savepath
+
+        return 0
  
     def is_valid_parameters(self, requested_item:dict):
         if is_request_valid(requested_item)==0: return 0
