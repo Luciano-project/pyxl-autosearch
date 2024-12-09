f9fcd185551c51b538cec7c6134b6fa7974402c8
Luciano-project <luhugo.lhcs@gmail.com>
2024-12-09 16:34:01 +0000

    feat: added a validation to initiate application in debug mode

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 from api.routes import api
-
+import os
+from dotenv import load_dotenv
 import sys
 
+load_dotenv()
+ 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and "-d" in sys.argv:
+    if (len(sys.argv) > 1 and "-d" in sys.argv) or os.getenv("DEBUG") == "True":
         api.run(host='0.0.0.0', port=5000, debug=True)        
     else:
         from waitress import serve
-        print("\n\tServidor de produção iniciado com sucesso em http://localhost:5000")
+        print("\n\tProductions server initalized on: http://localhost:5000")
         serve(api, host='0.0.0.0', port=5000)
 
